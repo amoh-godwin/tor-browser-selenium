b7c7e97e4c6d2b1abdb8d0e7591a8eb1475101df
amoh-godwin <amohgyebigodwin@gmail.com>
2020-12-22 16:23:37 +0000

    travis_win

---
+++ travis_win.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+killall tor
+mkdir -p ${DOWNLOAD_DIR}
+echo $VERSION_ARCH
+echo ${TBB_DIST_URL}/${VERSION_ARCH}
+[ ! -e ${DOWNLOAD_DIR}/$TARBALL ] && wget -P ${DOWNLOAD_DIR} ${TBB_DIST_URL}/${VERSION_ARCH}
+tar -xf ${DOWNLOAD_DIR}/$TARBALL -C $HOME
+echo `which curl`
+
+geckodriver_url="https://github.com/mozilla/geckodriver/releases/download/v0.27.0/geckodriver-v0.27.0-linux64.tar.gz"
+geckodriver_tarball=`basename ${geckodriver_url}`
+echo $geckodriver_tarball
+wget -P ${DOWNLOAD_DIR} $geckodriver_url
+tar -xf ${DOWNLOAD_DIR}/$geckodriver_tarball -C $HOME
+export PATH=$PATH:$HOME
